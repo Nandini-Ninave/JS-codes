f76bb7cda3c8a1b314374fd6383c235ef9a7b731
Nandini-Ninave <nandinininave99@gmail.com>
2026-06-25 19:28:37 +0530

    Commented previous code and wrote code to handle function callback

--- callback.js
+++ callback.js
@@ -1,8 +1,21 @@
-function fun1(callback){
-    console.log("fun1")
-    callback()
+// function fun1(callback){
+//     console.log("fun1")
+//     callback()
+// }
+// function fun2(){
+//     console.log("fun2")
+// }
+// fun1(fun2)
+
+//Callback functional handling
+function calculation(a, b, callback){
+    return callback(a,b)
 }
-function fun2(){
-    console.log("fun2")
+function add(a,b){
+    console.log(a+b)
 }
-fun1(fun2)
+function mul(a,b){
+    console.log(a*b)
+}
+calculation(2, 3, add)
+calculation(2, 3, mul)
