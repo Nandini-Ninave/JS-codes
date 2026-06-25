614f8c9564b3000bd473225b58e3436015a1cf80
Nandini-Ninave <nandinininave99@gmail.com>
2026-06-25 14:34:40 +0530

    Added file to practice class

---
+++ class.js
@@ -0,0 +1,13 @@
+class Std{
+    constructor(name, age){
+        this.name = name
+        this.age = age
+    }
+    study(){
+        console.log(`${this.name} studying`)
+    } 
+}
+const std1 = new Std("std1", 9)
+const std2 = new Std("std2", 20)
+std1.study()
+std2.study()
