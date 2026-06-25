7742813421dc6398733a57aefa10936db9dae48d
Nandini-Ninave <nandinininave99@gmail.com>
2026-06-25 15:49:22 +0530

    skipping value using destructuring

--- arraydestructuring.js
+++ arraydestructuring.js
@@ -1,11 +1,16 @@
 let arr = [1,2,3,4]
-const [arr0, arr1, arr2, arr3] = arr
-console.log(arr0, arr1, arr2, arr3)
+// const [arr0, arr1, arr2, arr3] = arr
+// console.log(arr0, arr1, arr2, arr3)
 
 //swapping using destructuring
 
-const arr = [1,2,3];
-let [arr0, arr1] = arr;
-console.log(arr0,arr1);
-[arr0,arr1] = [arr1,arr0];
-console.log(arr0, arr1);
+// const arr = [1,2,3];
+// let [arr0, arr1] = arr;
+// console.log(arr0,arr1);
+// [arr0,arr1] = [arr1,arr0];
+// console.log(arr0, arr1);
+
+//skipping value
+
+const[, arr1,arr2] = arr
+console.log(arr1,arr2)
