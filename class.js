a4242aa9e3a24bdd2a04400b24f4c999beed89cb
Nandini-Ninave <nandinininave99@gmail.com>
2026-06-25 14:42:18 +0530

    Changed code to check Inheritance

--- class.js
+++ class.js
@@ -7,9 +7,19 @@ class Std{
         console.log(`${this.name} studying`)
     } 
 }
+class Emp extends Std{
+    constructor(name){
+        super(name)
+    }
+    work(){
+        console.log(`${this.name} works`) 
+    }
+}
 const std1 = new Std("std1", 9)
 const std2 = new Std("std2", 20)
 std1.study()
 std2.study()
 console.log(std1.name)
 console.log(std2.age)
+const emp1 = new Emp("emp1")
+emp1.work()
