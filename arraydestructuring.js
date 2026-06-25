5a30f55161cda726b0ec0602673f23a309cd0e5c
Nandini-Ninave <nandinininave99@gmail.com>
2026-06-25 15:42:58 +0530

    Added swapping code

--- arraydestructuring.js
+++ arraydestructuring.js
@@ -1,3 +1,11 @@
 let arr = [1,2,3,4]
 const [arr0, arr1, arr2, arr3] = arr
 console.log(arr0, arr1, arr2, arr3)
+
+//swapping using destructuring
+
+const arr = [1,2,3];
+let [arr0, arr1] = arr;
+console.log(arr0,arr1);
+[arr0,arr1] = [arr1,arr0];
+console.log(arr0, arr1);
