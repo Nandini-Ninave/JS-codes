a11e09b173a49258e61aa84e8aefc98cdcb825d1
Nandini-Ninave <nandinininave99@gmail.com>
2026-06-26 17:00:20 +0530

    Commented previous code and added new code

--- call.js
+++ call.js
@@ -1,8 +1,23 @@
-let x ={name:"abc"}
-let person={
-    name:"xyz",
-    say : function(a){
-        console.log(`${this. name} - ${a}`)
-    }
+// let x ={name:"abc"}
+// let person={
+//     name:"xyz",
+//     say : function(a){
+//         console.log(`${this. name} - ${a}`)
+//     }
+// }
+// person.say.call(x, 9)
+
+function fun1(name, age){
+    this.name = name
+    this.age = age
 }
-person.say.call(x, 9)
+function emp(){
+    fun1.call(this, "xyz", 20)
+    console.log("emp - ", this)
+}
+function std(){
+    fun1.call(this, "abc", 10)
+    console.log("std - ", this)
+}
+const x = new emp()
+const y = new std()
